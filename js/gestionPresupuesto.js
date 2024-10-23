2241f59f46e8b37d9c879f67da90b6adc28c631d
PJulianP94 <julperper4@alu.edu.gva.es>
2024-10-23 20:25:01 +0200

    Remoto profesor agregadO

--- js/gestionPresupuesto.js
+++ js/gestionPresupuesto.js
@@ -1,19 +1,56 @@
 // TODO: Crear las funciones, objetos y variables indicadas en el enunciado
 
 // TODO: Variable global
+presupuesto = 0;
 
 
-function actualizarPresupuesto() {
-    // TODO
-}
-
+function actualizarPresupuesto(nuevopresupuesto) {
+    if (nuevopresupuestopresupuesto => 0)
+    {
+        nuevopresupuesto = presupuesto;
+        return presupuesto;
+    }      
+    else
+    {
+        return -1;
+        console.log ("El presupuesto es negativo")
+    }
+        }
+    
 function mostrarPresupuesto() {
-    // TODO
+   console.log ("Tu presupuesto actual es de: " + presupuesto)
 }
 
 function CrearGasto() {
-    // TODO
-}
+    let Gasto = 
+    {
+        descripcion: "" ,
+        valor: "",
+        mostrarGasto : function ()
+        {
+            console.log(`Gasto correspondiente a ${this.descripcion} con valor ${this.valor} €`);
+        },
+        actualizarDescripcion: function (nuevadescripcion)
+        {
+            this.descripcion = nuevadescripcion;
+        },
+        actualizarValor: function (nuevovalor)
+        {
+            if (nuevovalor >= 0)
+            {
+                this.valor = nuevovalor;
+            }
+            
+        else 
+        {
+            console.log ("El valor no puede ser negativo")
+        }
+     
+        },
+    }
+    return Gasto;
+    }
+
 
 // NO MODIFICAR A PARTIR DE AQUÍ: exportación de funciones y objetos creados para poder ejecutar los tests.
 // Las funciones y objetos deben tener los nombres que se indican en el enunciado
